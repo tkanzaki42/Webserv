2098d8a6cd8a8284cc1c955503aa10326ef3ba6e
harinez2 <yonishi@student.42tokyo.jp>
2022-09-08 00:51:16 +0900

    simple hello world server

---
+++ includes/webserv.hpp
@@ -0,0 +1,13 @@
+#ifndef WEBSERVER_HPP
+#define WEBSERVER_HPP
+
+#include <iostream>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sstream>
+
+#endif
