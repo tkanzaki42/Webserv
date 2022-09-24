2cf8725ddfbeeed69e2b92bb421bacf4d8dac18c
tkanzaki <tkanzaki@student.42tokyo.jp>
2022-09-24 02:36:55 +0200

    FDManager追加

---
+++ srcs/server/FDManager.cpp
@@ -0,0 +1,64 @@
+#include "srcs/server/FDManager.hpp"
+#include "srcs/server/Webserv.hpp"
+
+FDManager::FDManager():
+accept_fd_(-1),
+socket_(HTTP_PORT) {
+}
+
+FDManager::~FDManager() {
+}
+
+FDManager::FDManager(const FDManager &obj):
+accept_fd_(-1),
+socket_(HTTP_PORT) {
+    *this = obj;
+}
+
+FDManager &FDManager::operator=(const FDManager &obj) {
+    accept_fd_     = obj.accept_fd_;
+    return *this;
+}
+
+int FDManager::get_accept_fd() const {
+    return accept_fd_;
+}
+
+bool FDManager::accept() {
+    accept_fd_ = ::accept(socket_.get_listen_fd(),
+        (struct sockaddr*)NULL, NULL);
+    return accept_fd_;
+    if (accept_fd_ == -1) {
+        return false;
+    }
+    return true;
+}
+
+bool FDManager::send(const std::string &str) const {
+    if (::send(accept_fd_, str.c_str(),
+        str.length(), 0) == -1) {
+        return false;
+    }
+    return true;
+}
+
+int FDManager::recieve(char buf[]) const {
+    memset(buf, 0, sizeof(char) * BUF_SIZE);
+    int read_size = recv(accept_fd_,
+        buf, sizeof(char) * BUF_SIZE - 1, 0);
+    buf[read_size] = '\0';
+    // std::cout << "!this!" << buf << std::endl;
+    return read_size;
+}
+
+void FDManager::disconnect() const {
+    close(accept_fd_);
+}
+
+void FDManager::create_socket() {
+    socket_.prepare();
+}
+
+void FDManager::destory_socket() {
+    socket_.cleanup();
+}
---
+++ srcs/server/FDManager.hpp
@@ -0,0 +1,43 @@
+
+#ifndef FD_MANAGER_HPP_
+#define FD_MANAGER_HPP_
+
+#include <iostream>
+#include <string>
+#include "srcs/util_network/Socket.hpp"
+
+class Webserv;
+
+class FDManager {
+ private:
+    int                 accept_fd_;
+    Socket              socket_;
+
+ public:
+    FDManager();
+    ~FDManager();
+    FDManager(const FDManager &obj);
+    FDManager &operator=(const FDManager &obj);
+
+    int get_accept_fd() const;
+
+    // ファイルディスクリプタをオープン(クライアント)
+    bool accept();
+
+    // ファイルディスクリプタをクローズ(クライアント)
+    void disconnect() const;
+
+    // ファイルディスクリプタに書き込む(クライアント)
+    bool send(const std::string &str) const;
+
+    // ファイルディスクリプタを読み込む(クライアント)
+    int recieve(char buf[]) const;
+
+    // ソケットを作成(ソケット)
+    void create_socket();
+
+    // ソケットを削除(ソケット)
+    void destory_socket();
+};
+
+#endif  // PROJ_WEBSERV_SRCS_SERVER_FDMANAGER_HPP_
