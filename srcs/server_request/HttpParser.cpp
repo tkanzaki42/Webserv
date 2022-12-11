5d15d173a2d5c11e6b4e2d2f178b0515ae02fbbe
chobikaoru <82760906+chobikaoru@users.noreply.github.com>
2022-12-12 01:59:36 +0900

    separate_querystring_pathinfo()のバグ修正

--- srcs/server_request/HttpParser.cpp
+++ srcs/server_request/HttpParser.cpp
@@ -164,7 +164,7 @@ void HttpParser::separate_querystring_pathinfo() {
     std::string remaining_path = split_query_string_(request_target_);
 
     // path_to_file_を仮で設定
-    path_to_file_ = this->baseHtmlPath + remaining_path;
+    path_to_file_ += remaining_path;
 
     // パスからPATH_INFOを切り出す
     if (get_http_method() != METHOD_POST)
