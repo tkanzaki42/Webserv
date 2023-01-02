40a270264741b99f3f1fa82f74193e42a3709201
chobikaoru <82760906+chobikaoru@users.noreply.github.com>
2023-01-03 01:21:49 +0900

    add testcases

--- testcases/403_ng.sh
+++ testcases/403_ng.sh
@@ -1,7 +1,8 @@
-
+# ./webserv configs/valid/01_host.conf
+# brew services start nginx (autoindex / indexの設定をしないでサーバー起動)
 echo "~~~~~nginx~~~~~"
-curl -v -X "GETT" localhost
+curl -v localhost:8080/
 echo;
 echo;
 echo "~~~~~webserv~~~~~"
-curl -v -X "GETT" localhost:5050
+curl -v localhost:5050/
--- testcases/403_ok.sh
+++ testcases/403_ok.sh
@@ -1,7 +1,8 @@
-
+# ./webserv configs/valid/13_autoindex.conf
+# brew services start nginx (autoindexの設定をonでサーバー起動)
 echo "~~~~~nginx~~~~~"
-curl -v -X "GETT" localhost
+curl -v localhost/
 echo;
 echo;
 echo "~~~~~webserv~~~~~"
-curl -v -X "GETT" localhost:5050
+curl -v localhost:5050/
