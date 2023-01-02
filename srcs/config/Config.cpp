d71e4d7c8a8de8ffe5a3d559d6bffdda1199fd68
chobikaoru <82760906+chobikaoru@users.noreply.github.com>
2023-01-02 21:38:19 +0900

    上に同じ

--- srcs/config/Config.cpp
+++ srcs/config/Config.cpp
@@ -142,7 +142,7 @@ void    Config::parseConfig(const std::string &path) {
         }
     }
     // デバッグ用 : コンフィグの中身を全て出力する
-    Config::printConfig();
+    // Config::printConfig();
     ifs.close();
     if (!ConfigChecker::isValidConfig())
         throw(Config::ConfigFormatException());
